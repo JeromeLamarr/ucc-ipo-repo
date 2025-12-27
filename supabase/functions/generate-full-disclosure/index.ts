36ddff515d5eda033c8e78b907fd576242414704
lamarr0409 <jeromelamarr0409@gmail.com>
2025-12-27 16:10:24 +0800

    disclosure abstract content adjusted

--- supabase/functions/generate-full-disclosure/index.ts
+++ supabase/functions/generate-full-disclosure/index.ts
@@ -322,7 +322,7 @@ async function generateFullDisclosurePDF(
   if (ipRecord.abstract) {
     page.drawText("ABSTRACT", { x: margin + 25, y: yPosition, size: 10, color: accentColor, fontStyle: "bold" });
     yPosition = moveDown(yPosition, 14);
-    page.drawText(ipRecord.abstract, { x: margin + 25, y: yPosition, size: 8, color: darkColor, maxWidth: contentWidth - 200, lineHeight: 5 });
+    page.drawText(ipRecord.abstract, { x: margin + 25, y: yPosition, size: 8, color: darkColor, maxWidth: contentWidth - 200, lineHeight: 15 });
     yPosition = moveDown(yPosition, 28);
   }
 
